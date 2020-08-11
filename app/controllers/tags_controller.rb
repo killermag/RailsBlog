f2e1787a4109d8d39938610abfd9ff1b21e679a6
killermag <galahaal580@gmail.com>
2020-08-11 21:50:38 +0300

    railsblog done

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+
+  before_action :require_login , only: [:destroy] 
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index 
+    @tags = Tag.all 
+  end 
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to articles_path
+  end
+  
+end
